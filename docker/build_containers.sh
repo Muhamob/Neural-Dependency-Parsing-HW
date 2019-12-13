fd968c9eb1f3565436d095d68c42e57f9576c1a5
muhamob <yusov.aa@phystech.edu>
2019-12-13 13:23:53 +0300

    Rearrange directory structure

--- docker/build_containers.sh
+++ docker/build_containers.sh
@@ -1,9 +1,5 @@
 echo "build kitaev model"
-mv Dockerfile_kitaev Dockerfile
-docker build -t hw/kitaev .
-mv Dockerfile Dockerfile_kitaev
+docker build -t hw/kitaev ./kitaev
 
 echo "build dozat model"
-mv Dockerfile_dozat Dockerfile
-docker build -t hw/dozat .
-mv Dockerfile Dockerfile_dozat
+docker build -t hw/dozat ./dozat
